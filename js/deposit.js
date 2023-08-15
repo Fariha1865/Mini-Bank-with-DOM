6b5d40bf11a1b1ec64195114296d8ad665f07f74
Fariha1865 <farihaamin187@gmail.com>
2023-08-15 12:40:46 +0600

    deposit amount added with main balance

--- js/deposit.js
+++ js/deposit.js
@@ -15,14 +15,20 @@ document.getElementById('btn_deposite').addEventListener('click',function(){
             alert('Please enter some amount to deposit')
             return;
 
+          }else if(depositInput.value < 0)
+          {
+            depositInput.value = '';
+            alert('You cannot enter negative amount to deposit');
+            return;
           }
 
           var depositTotal = parseFloat(depositBoxValue.innerText)
           depositTotal += parseFloat(depositAmount)
           depositBoxValue.innerText = depositTotal;
         
-          j += parseInt(depositAmount);
-          balanceBoxValue.innerText= j;
+          var balanceTotal = parseFloat(balanceBoxValue.innerText)
+          balanceTotal += parseFloat(depositAmount)
+          balanceBoxValue.innerText = balanceTotal;
           depositInput.value = '';
           
           
